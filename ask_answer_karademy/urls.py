47fe1a4434ae9cca07ab8322e231c35943ce731d
amir mohammadian <amirmohammadian@hotmail.com>
2020-12-19 19:20:22 +0330

    adding urls

--- ask_answer_karademy/urls.py
+++ ask_answer_karademy/urls.py
@@ -1,21 +1,15 @@
-"""ask_answer_karademy URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views as question_website_views
+from users import views as users_views
+from question import views as question_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("about/", question_website_views.about ,name="about"),
+    path("", include("question.urls")),
+    path("register/", users_views.register, name="register"),
+    path("ask_question/", question_views.AskQuestion.as_view(), name="ask_question"),
+    path("question_detail/<int:pk>", question_views.QuestionDetail.as_view(), name="question_detail"),
 ]
