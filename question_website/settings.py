a63b6707f0bbe8e8183a88f341451e581422d9aa
amir mohammadian <amirmohammadian@hotmail.com>
2020-12-20 09:56:55 +0330

    adding apps in setting such as ckeditor, crispy forms

--- question_website/settings.py
+++ question_website/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "question.apps.QuestionConfig",
+    "crispy_forms",
+    "ckeditor",
 ]
 
 MIDDLEWARE = [
