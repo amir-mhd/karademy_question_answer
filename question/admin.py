8816ee234ef358bb498fbf64ce5baa7508b86856
amir mohammadian <amirmohammadian@hotmail.com>
2020-12-20 10:42:20 +0330

    adding question app admin

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question, Answer, Tag, Category
 
-# Register your models here.
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Tag)
+admin.site.register(Category)
+
