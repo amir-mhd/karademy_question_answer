217ddc61c1ae4b1d5857090066f8518afdcb2d36
amir mhd <amirmohammadian@hotmail.com>
2020-12-23 12:55:19 +0330

    adding the views and forms

--- question/views.py
+++ question/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Question 
 
-# Create your views here.
+class HomeListView(ListView):
+    model = Question
+    template_name = "question/questions.html"
+    
+
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = 'question/question_detail.html'
+
+
+class QuestionCreateView(CreateView):
+    model = Question
+    form_class = QuestionForm
+    template_name = 'question/ask_question.html'
