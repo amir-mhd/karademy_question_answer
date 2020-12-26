ff90d771f72117624f60fb144973acab3900af1d
amir mhd <amirmohammadian@hotmail.com>
2020-12-26 21:22:11 +0330

    static and media conf in settings

--- question_website/settings.py
+++ question_website/settings.py
@@ -124,5 +124,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
