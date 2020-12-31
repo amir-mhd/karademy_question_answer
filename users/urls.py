4c92fe53b47ee0500d39c9cae651bea253c3d0f4
amir mhd <amirmohammadian@hotmail.com>
2020-12-31 18:59:47 +0330

    creating the log out page

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 ] 
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from .forms import UserRegisterForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
     template_name = "users/register.html"
     success_url = reverse_lazy("login")
+    success_message = f"{User.username} was created successfully"
