dd8c1472b0db0fc7630bfa53db23349fec8ac140
amir mhd <amirmohammadian@hotmail.com>
2021-01-02 21:20:31 +0330

    trying to add the message after registeration (not working yet)

--- users/views.py
+++ users/views.py
@@ -5,8 +5,15 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.models import User
 
 
+
 class UserRegisterView(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
     template_name = "users/register.html"
     success_url = reverse_lazy("login")
-    success_message = f"{User.username} was created successfully"
+    # success_message = f"{username} was created successfully"
+    # success_message = f"{User.get_username(self)} was created successfully"
+    # def success_message(self):
+    #     username = get_username(self.User)
+    #     return f"{username} was created successfully"
+    # def get_success_message(self, cleaned_data):
+    #     return self.f"{username} was created sucessfully" % dict(cleaned_data, username=self.object.username,)
