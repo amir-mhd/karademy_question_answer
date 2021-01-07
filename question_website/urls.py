12c4b83d1df8a3a1a4829c5c0e9dda43447d6f4b
amir mhd <amirmohammadian@hotmail.com>
2021-01-07 16:30:37 +0330

    creating user profile

--- question_website/urls.py
+++ question_website/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views as question_website_views
@@ -8,5 +10,8 @@ urlpatterns = [
     path("about/", question_website_views.AboutTemplateView.as_view() ,name="about"),
     path("", include("question.urls")),
     path("users/", include("users.urls")),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
