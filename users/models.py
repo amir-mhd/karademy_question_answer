2e8315ee203d46452403a79e3e5fa7f2f768b15e
amir mhd <amirmohammadian@hotmail.com>
2021-01-07 15:26:24 +0330

    profile registeration in admin and class Meta in models

--- users/models.py
+++ users/models.py
@@ -6,6 +6,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_imgs")
 
+    class Meta:
+        db_table = "Profile"
+        verbose_name_plural = "Profiles"
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    
