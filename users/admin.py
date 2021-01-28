be42a317ed6de64516e9ce1ff3fe615cbe49eef0
amir mhd <amirmohammadian@hotmail.com>
2021-01-28 12:09:13 +0330

    changes in admin panle profile

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+# admin.site.register(Profile)
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "git_url")
+    search_fields = ("user", "bio", "git_url")
+    raw_id_fields = ("user",)    
